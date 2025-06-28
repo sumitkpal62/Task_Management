061a1e7e0126e31ce03489f87f35e21f67d63c43
Sumit <sciencebuddy51@gmail.com>
2025-06-28 13:47:35 +0530

    Starting page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectPage from "./components/NoProjectPage";
 import ProjectSection from "./components/ProjectSection";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+
+  function handleStartAppProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProject === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProject === undefined) {
+    content = <NoProjectPage onStartAddProject={handleStartAppProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSection />
-      <NewProject />
+      <ProjectSection onStartAddProject={handleStartAppProject} />
+      {content}
     </main>
   );
 }
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,13 +1,18 @@
-const ProjectSection = () => {
+import Button from "./Button";
+
+const ProjectSection = ({ onStartAddProject }) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600">
+        <Button
+          onClick={onStartAddProject}
+          className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600"
+        >
           + Add Projects
-        </button>
+        </Button>
       </div>
     </aside>
   );
